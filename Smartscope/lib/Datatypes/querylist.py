28233a365b7f13ea5363927987575e29e7ab9819
Jonathan <jonathan.bouvette@nih.gov>
2023-09-29 19:08:00 +0000

    added post request

--- Smartscope/lib/Datatypes/querylist.py
+++ Smartscope/lib/Datatypes/querylist.py
@@ -1,8 +1,12 @@
-from typing import Optional, Any
+from typing import Optional, Any, List
 
 
 class QueryList(list):
+    #List of Pydantic models
 
     def first(self) -> Optional[Any]:
         if len(self) > 0:
             return self[0]
+        
+    def dump_all(self) -> List[Any]:
+        return [obj.model_dump() for obj in self]
